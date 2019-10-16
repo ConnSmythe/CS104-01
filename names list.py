584dd5e2ad556a1036362f7bf9a2c567f1d91994
ConnSmythe <55414267+ConnSmythe@users.noreply.github.com>
2019-10-16 16:26:02 -0400

    Add files via upload

---
+++ names list.py
@@ -0,0 +1,16 @@
+#List creation and searching
+names  = []
+for x in range(0,10):
+    aname = input("enter a name:")
+    names.append(aname)
+    
+    
+
+print (names)
+search = input("enter search name:")
+if search in names:
+    print(search, "was found")
+    
+else:
+    print (search, "was not found")
+    
