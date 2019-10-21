2d0fcfc0fa10b5b5306be8e67f41a94bea94df4b
ConnSmythe <55414267+ConnSmythe@users.noreply.github.com>
2019-10-21 11:10:56 -0400

    Update names list.py

--- names list.py
+++ names list.py
@@ -1,16 +1,19 @@
 #List creation and searching
+end=False
 names  = []
 for x in range(0,10):
     aname = input("enter a name:")
     names.append(aname)
     
     
-
-print (names)
-search = input("enter search name:")
-if search in names:
-    print(search, "was found")
+while not end:
     
-else:
-    print (search, "was not found")
+    print (names)
+    search = input("enter search name:")
+    if search == ("end"):
+        end=True
+    elif search in names:
+        print(search, "was found")
     
+    elif search not in names:
+        print (search, "was not found")
